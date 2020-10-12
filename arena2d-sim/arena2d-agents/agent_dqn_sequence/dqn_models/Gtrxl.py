2010ab7302ad326e84d776fa96f3cb11fb9a6a79
Junhui Li <junhuili9408@gmail.com>
2020-10-12 13:29:03 +0200

    Update Gtrxl.py
    ..

--- arena2d-sim/arena2d-agents/agent_dqn_sequence/dqn_models/Gtrxl.py
+++ arena2d-sim/arena2d-agents/agent_dqn_sequence/dqn_models/Gtrxl.py
@@ -35,7 +35,7 @@ class TransformerDqn(nn.Module):
 		# sequences with different lengths
                 embedding=self.embedding(x.data)                
                 x_transform, new_mem = self.transformer(embedding, mem)
-				print("x_transform",x_transform)
+				print("x_transform",x_transform);
                 q_vals = self.linear(x_transform[-1])
                 return q_vals, new_mem
                 
